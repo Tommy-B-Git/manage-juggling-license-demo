00e804ed5ef5d3db0a0b53fb18216fa0eb9bc89f
Tom <thomas.h.brown@icloud.com>
2024-04-24 15:48:51 +0100

    Added random statuses with fake.helper.arrayElements in generate-applications.js

--- app/routes/applications.js
+++ app/routes/applications.js
@@ -1,7 +1,7 @@
 module.exports = router => {
 
  router.get('/applications/:applicationId', (req,res) => {
-   // To get an individual Id use : in the path (see above)
+   // To GET an individual Id use : in the path (see above)
    // req.session.data.applications - all the data
    //req.params.applicationId - to get at the Id in the query string
    let application = req.session.data.applications.find(application => application.id === req.params.applicationId)
